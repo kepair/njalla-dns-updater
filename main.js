425554fbdeb7dc54116fadc32bcaf2beb0a20850
kepair <pedireg@gmail.com>
2020-11-14 11:56:50 +0100

    fixing A records

--- main.js
+++ main.js
@@ -11,21 +11,22 @@ const currentPath = process.argv[2];
     var credentials = fs.readFileSync(currentPath + '/credentials.txt').toString().split("\n");
     //Getting server public IP
     var pubIp = await publicIp.v4()
-        //Log in to Nja.la
+    //Log in to Nja.la
     await login(credentials[0], credentials[1])
-        //Get all domains for that account
+    //Get all domains for that account
     const domains = await getDomains()
-        //Update IP's of all records from all domains
-    for (i = 0; i < domains.length; i++) {
-        const records = await getRecords(domains[i])
-        for (j = 0; j < records.length; j++) {
+    //Update IP's of all records from all domains
+    domains.forEach(async domain => {
+        const records = await getRecords(domain);
+        const aRecords = records.filter(record => record.type == 'A');
+        aRecords.forEach(async record => {
             //If IP is not the same, update records
-            if (pubIp != records[j].content) {
-                await update(domains[i], records[j], { content: pubIp })
-                console.log(records[j].name + "." + domains[i] + " new ip: " + pubIp)
+            if (pubIp != record.content) {
+                await update(domain, record, { content: pubIp })
+                console.log(record.name + "." + domain + " new ip: " + pubIp)
             } else {
-                console.log(records[j].name + "." + domains[i] + " ip is up to date")
+                console.log(record.name + "." + domain + " ip is up to date")
             }
-        }
-    }
+        });
+    });
 })()
